585ec9ad5bc9cd7bd4a05cb1d1971d6613ad3e3b
Nelson Gallego <nelson@remotepcsolutions.com>
2022-10-25 21:45:28 -0500

    some change

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ const morgan = require('morgan')
 const cors = require('cors')
 const compression = require('compression')
 const helmet = require('helmet')
+const path = require('path')
+
+// const m3u8ToMp4 = require("m3u8-to-mp4");
+// const converter = new m3u8ToMp4();
 
 const app = express()
 
 app.use(express.json()).use(cors()).use(helmet()).use(compression()).use(morgan('dev'))
 
+// converter
+// .setInputFile('https://phenixrts.com/video/grandest.corp/us-west%23us-west1-c.da8lUcVf.20221004.PSCk5VkF/vod.m3u8')
+// .setInputFile(__dirname)
+// .start()
+
+console.log(path.join(__dirname))
+
 app.get('/api', (req, res) => {
-    res.json({
+    res.json({ 
         msg: 'all good :)'
     })
 })
