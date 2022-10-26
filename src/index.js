b82c882f1023dc08ae4a3ec7c57a2fc61fd85388
Nelson Gallego <nelson@remotepcsolutions.com>
2022-10-25 22:19:56 -0500

    add test download hls

--- src/index.js
+++ src/index.js
@@ -5,24 +5,22 @@ const compression = require('compression')
 const helmet = require('helmet')
 const path = require('path')
 
-const m3u8ToMp4 = require("m3u8-to-mp4");
-const converter = new m3u8ToMp4();
+const download = require("node-hls-downloader").download;
 
 const app = express()
 
 app.use(express.json()).use(cors()).use(helmet()).use(compression()).use(morgan('dev'))
 
-converter
-.setInputFile('https://phenixrts.com/video/grandest.corp/us-west%23us-west1-c.da8lUcVf.20221004.PSCk5VkF/vod.m3u8')
-.setInputFile('/home/ubuntu/bucket-test-grandest/test.mp4')
-.start()
+app.get('/api', async (req, res) => {
+    console.log(path.join(__dirname))
+    // await download({
+    //     quality: "best",
+    //     concurrency: 5,
+    //     outputFile: path.join(__dirname, 'videoo.mp4'),
+    //     streamUrl: "https://phenixrts.com/video/grandest.corp/southamerica-east%23sa-saopaulo-1-ad-1.fnvarOMH.20220921.PScd57Lw/vod.m3u8",
+    // });
 
-console.log(path.join(__dirname))
-
-app.get('/api', (req, res) => {
-    res.json({ 
-        msg: 'all good :)'
-    })
+    res.json({ msg: 'all good :)' })
 })
 
 app.listen(3000, () => {
