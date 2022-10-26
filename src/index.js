2cad9e57259de99f1716086cdbf38438adcd3a95
Nelson Gallego <nelson@remotepcsolutions.com>
2022-10-25 23:15:40 -0500

    add folder with id

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const compression = require('compression')
 const helmet = require('helmet')
 const path = require('path')
+const fs = require('fs')
 
 const download = require("node-hls-downloader").download;
 
@@ -14,10 +15,14 @@ const app = express()
 app.use(express.json()).use(cors()).use(helmet()).use(compression()).use(morgan('dev'))
 
 app.get('/api', async (req, res) => {
+    
+    const folder = v4()
+    fs.mkdirSync(path.join(__dirname, `../../../bucket-test-grandest/${folder}`))
+
     await download({
         quality: "best",
         concurrency: 5,
-        outputFile: (path.join(__dirname, `../../../bucket-test-grandest/${v4()}/video.mp4`)),
+        outputFile: (path.join(__dirname, `../../../bucket-test-grandest/${folder}/video.mp4`)),
         streamUrl: "https://phenixrts.com/video/grandest.corp/southamerica-east%23sa-saopaulo-1-ad-1.fnvarOMH.20220921.PScd57Lw/vod.m3u8",
     });
 
