b187dc7667dcb677ab86cde7dbe29fc790921b43
Nelson Gallego <nelson@remotepcsolutions.com>
2022-10-27 13:22:04 -0500

    add funciont download

---
+++ src/functionDownload.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const request = require('request')
+
+function download(url, isBinary) {	
+    
+    return new Promise((resolve, reject) => {
+        let requestSettings = {
+            url: url,
+            method: 'GET'
+        }
+        if (isBinary) requestSettings.encoding = null;
+        request(requestSettings, function (error, response, body){
+            if (error) {
+                reject(error);
+            } else {
+                let urlres = url.match(/^(.*\/)(.*)$/)
+                let basename = urlres[2]
+                fs.writeFile( path.join(__dirname, `../../../bucket-test-grandest/${folder}`, basename) , body, (err) => {});
+                // fs.writeFile( basename , body, (err) => {});
+                resolve(body);
+            }
+        })		
+    })
+}
+
+module.exports = download
--- src/index.js
+++ src/index.js
@@ -5,28 +5,72 @@ const compression = require('compression')
 const helmet = require('helmet')
 const path = require('path')
 const fs = require('fs')
+const download = require('./functionDownload')
 
-const download = require("node-hls-downloader").download;
-
-const { v4 } = require('uuid')
 
 const app = express()
 
 app.use(express.json()).use(cors()).use(helmet()).use(compression()).use(morgan('dev'))
 
-app.get('/api', async (req, res) => {
-    
-    const folder = v4()
+app.get('/api/:livestreamId/:streamId', async (req, res) => {
+
+    const { livestreamId, streamId } = req.params
+
+    const folder = livestreamId
     fs.mkdirSync(path.join(__dirname, `../../../bucket-test-grandest/${folder}`))
 
-    await download({
-        quality: "best",
-        concurrency: 5,
-        outputFile: (path.join(__dirname, `../../../bucket-test-grandest/${folder}/video.mp4`)),
-        streamUrl: "https://phenixrts.com/video/grandest.corp/southamerica-east%23sa-saopaulo-1-ad-1.fnvarOMH.20220921.PScd57Lw/vod.m3u8",
-    });
+    
+    let parallel = 5;
+    
+    // https://phenixrts.com/video/grandest.corp/southamerica-east%23sa-saopaulo-1-ad-1.hduF7wk9.20220920.PSdbNrWt/vod.m3u8
+    
+    async function main(streamId) {
+        console.log("🚀 ~ file: index.js ~ line 28 ~ main ~ streamId", streamId)
+        // if (process.argv.length < 3) {
+        //     console.log("ERROR")
+        //     process.exit(1);
+        // }
+    
+        // if (process.argv.length >= 4) {
+        //     parallel = process.argv[3]*1;
+        // }
+    
+        // let url = process.argv[2];
+        const url = streamId
+    
+        let urlres = url.match(/^(.*\/)(.*)$/)
+        
+    
+        let pathname = urlres[1]
+        
+    
+        let body = await download(url).catch(err => console.log(err));
+        
+    
+        let resolutions = body.match(/^.*\.m3u8$/mg)
+        for(var res=0; res<resolutions.length; res++) {
+            let resolution = resolutions[res];
+            let body = await download(pathname + resolution).catch(err => console.log(err))
+    
+            let downloads = [];
+            let tsfiles = body.match(/^.*\.ts$/mg)
+            for(var ts=0; ts<tsfiles.length; ts++) {
+                let tsfile = tsfiles[ts]
+                console.log(pathname + tsfile)
+                downloads.push(download(pathname + tsfile, "binary"))
+                if (downloads.length >= parallel) {
+                    await Promise.all(downloads).then(() => {})
+                    downloads = [];
+                }
+            }
+        }
+    }
+    
+    // main('https://phenixrts.com/video/grandest.corp/' + encodeURIComponent(streamId) + '/vod.m3u8');
+    main('https://phenixrts.com/video/grandest.corp/us-northeast%23us-ashburn-ad-2.wNEq0bdZ.20221020.PSzwzsXD/vod.m3u8')
+    
 
-    res.json({ msg: 'all good :)' })
+    res.json({ msg: `https://bucket-test-grandest.s3.amazonaws.com/${livestreamId}/vod.m3u8` })
 })
 
 app.listen(3000, () => {
