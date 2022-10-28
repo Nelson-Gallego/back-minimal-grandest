f99fe1b6ac4cad2e99def558bc6b8082574dabd3
Nelson Gallego <nelson@remotepcsolutions.com>
2022-10-28 15:33:55 -0500

    add authentication

--- src/downloadM3U8.js
+++ src/downloadM3U8.js
@@ -1,61 +1,77 @@
 const request = require('request')
 const fs = require('fs')
 const path = require('path')
+const axios = require('axios').default
+const { config } = require('dotenv')
+config()
 
 function download(url, isBinary, folder) {
-	return new Promise((resolve, reject) => {
-		let requestSettings = {
-		    url: url,
-		    method: "GET"
-		}
-		if (isBinary) requestSettings.encoding = null
-		request(requestSettings, function (error, response, body){
-		    if (error) {
-		        reject(error)
-		    } else {
-				let urlres = url.match(/^(.*\/)(.*)$/)
-				let basename = urlres[2]
-				// fs.writeFile(basename, body, (err) => {})
-                fs.writeFile( path.join(__dirname, `../../../bucket-test-grandest/${folder}`, basename), body, (err) => {})
-		    	resolve(body)
-		    }
-		})		
-	})
+    return new Promise((resolve, reject) => {
+        let requestSettings = {
+            url: url,
+            method: "GET"
+        }
+        if (isBinary) requestSettings.encoding = null
+        request(requestSettings, function(error, response, body) {
+            if (error) {
+                reject(error)
+            } else {
+                let urlres = url.match(/^(.*\/)(.*)$/)
+                let basename = urlres[2]
+                // fs.writeFile(basename, body, (err) => {})
+                fs.writeFile(path.join(__dirname, `../../../bucket-test-grandest/${folder}`, basename), body, (err) => { })
+                resolve(body)
+            }
+        })
+    })
 }
 
 let parallel = 5
 
 async function startDownloadHLS(streamId, livestreamId) {
 
-    const urlStream = "https://phenixrts.com/video/grandest.corp/" + encodeURIComponent(streamId) + "/vod.m3u8"
+    try {
+        const urlStream = "https://phenixrts.com/video/grandest.corp/" + encodeURIComponent(streamId) + "/vod.m3u8"
 
-	let url = urlStream
+        const url = urlStream
 
-	let urlres = url.match(/^(.*\/)(.*)$/)
+        const urlres = url.match(/^(.*\/)(.*)$/)
 
-	let pathname = urlres[1]
+        let pathname = urlres[1]
 
-	let body = await download(url, undefined, livestreamId).catch(err => console.log(err))
+        let body = await download(url, undefined, livestreamId).catch(err => console.log(err))
 
-	let resolutions = body.match(/^.*\.m3u8$/mg)
-	for(var res=0; res<resolutions.length; res++) {
-		let resolution = resolutions[res]
-		let body = await download(pathname + resolution, undefined, livestreamId).catch(err => console.log(err))
+        let resolutions = body.match(/^.*\.m3u8$/mg)
+        for (var res = 0;res < resolutions.length;res++) {
+            let resolution = resolutions[res]
+            let body = await download(pathname + resolution, undefined, livestreamId).catch(err => console.log(err))
 
-		let downloads = []
-		let tsfiles = body.match(/^.*\.ts$/mg)
-		for(var ts=0; ts<tsfiles.length; ts++) {
-			let tsfile = tsfiles[ts]
-    		console.log(pathname + tsfile)
-    		downloads.push(download(pathname + tsfile, "binary", livestreamId))
-    		if (downloads.length >= parallel) {
-    			await Promise.all(downloads).then(() => {})
-    			downloads = []
-    		}
-    	}
+            let downloads = []
+            let tsfiles = body.match(/^.*\.ts$/mg)
+            for (var ts = 0;ts < tsfiles.length;ts++) {
+                let tsfile = tsfiles[ts]
+                console.log(pathname + tsfile)
+                downloads.push(download(pathname + tsfile, "binary", livestreamId))
+                if (downloads.length >= parallel) {
+                    await Promise.all(downloads).then(() => { })
+                    downloads = []
+                }
+            }
+        }
+
+        const tokenTemp = process.env.TOKEN_APP
+
+        await axios.put(`https://8236-143-137-96-129.ngrok.io/api/v1/livestreams/streamAvailable/${livestreamId}`, {
+            urlS3: `https://bucket-test-grandest.s3.amazonaws.com/${livestreamId}/vod.m3u8`
+        }, {
+            headers: {
+                Authorization: `Bearer ${tokenTemp}`
+            }
+        })
+
+    } catch (e) {
+        console.log(e)
     }
-
-    console.log('DESCARGA FINALIZADA ', streamId + ' ' + livestreamId)
 }
 
 module.exports = startDownloadHLS
--- src/index.js
+++ src/index.js
@@ -6,22 +6,33 @@ const helmet = require('helmet')
 const path = require('path')
 const fs = require('fs')
 const startDownloadHLS = require('./downloadM3U8')
+const axios = require('axios').default
+const { config } = require('dotenv')
+config()
 
 const app = express()
 
 app.use(express.json()).use(cors()).use(helmet()).use(compression()).use(morgan('dev'))
+
+app.get('/api', async (req, res) => {
+    try {
+        res.json('response backMinimal')
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 app.get('/api/:streamId/:livestreamId', async (req, res) => {
 
     const { streamId, livestreamId } = req.params
 
     const existFolder = fs.existsSync(path.join(__dirname, `../../../bucket-test-grandest/${livestreamId}`))
-    
-    if(!existFolder) fs.mkdirSync(path.join(__dirname, `../../../bucket-test-grandest/${livestreamId}`))
+
+    if (!existFolder) fs.mkdirSync(path.join(__dirname, `../../../bucket-test-grandest/${livestreamId}`))
 
     startDownloadHLS(streamId, livestreamId)
 
-    res.json({ msg: 'all good :)' })
+    res.status(200).json({ msg: 'Start download stream' })
 })
 
 app.listen(3000, () => {
